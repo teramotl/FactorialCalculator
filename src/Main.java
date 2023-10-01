155ba6c20fd9575a2cecefa6d66a1359f2b2e27a
Behrozbek Togayev <teramotl@icloud.com>
2023-10-01 18:33:40 +0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("enter number:");
+
+        int value = new Scanner(System.in).nextInt();
+
+        int factorial = 1;
+        for (int i = 1; i <= value; i = i + 1) {
+            factorial = factorial * i;
+        }
+
+        System.out.println("factorial: " + factorial);
+    }
+}
